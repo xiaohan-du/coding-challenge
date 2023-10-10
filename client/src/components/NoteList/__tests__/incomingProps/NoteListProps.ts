d19ef12b248799c74e019fe86d522c8274d2652d
Xiaohan Du <dux18@cardiff.ac.uk>
2023-10-10 21:15:19 +0100

    Improve incoming props

--- client/src/components/NoteList/__tests__/incomingProps/NoteListProps.ts
+++ client/src/components/NoteList/__tests__/incomingProps/NoteListProps.ts
@@ -2,19 +2,19 @@ export const NoteListProps = [
   {
     id: 1,
     createdAt: '2023-10-10T00:00:00.000Z',
-    user: 'User1',
-    note: 'Note 1',
+    user: 'test user 1',
+    note: 'test note 1',
   },
   {
     id: 2,
     createdAt: '2023-10-11T00:00:00.000Z',
-    user: 'User2',
-    note: 'Note 2',
+    user: 'test user 2',
+    note: 'test note 2',
   },
   {
     id: 3,
     createdAt: '2023-10-12T00:00:00.000Z',
-    user: 'User3',
-    note: 'Note 3',
+    user: 'test user 3',
+    note: 'test note 3',
   },
 ];
