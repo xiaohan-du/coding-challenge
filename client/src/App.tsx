5a0e757d167bfe67b62c491720bf865915507cb1
Xiaohan Du <dux18@cardiff.ac.uk>
2023-10-10 23:29:25 +0100

    Add function to fetch n months of data ahead of today

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
-import React, {useState} from "react";
+import React, {useState, useCallback} from "react";
 import "./App.css";
 import { useEffect } from "react";
 import NoteList from "./components/NoteList/NoteList";
 
 function App() {
   const [notesData, setNotesData] = useState([]);
-  useEffect(() => {
-    fetchData();
-  }, []);
 
-  const fetchData = async () => {
+  const calcDate = (nMonths: number) => {
+    const todaysDate = new Date();
+    const nMonthAheadDate = todaysDate.setMonth(todaysDate.getMonth() - nMonths);
+    const nMonthAheadDateFormatted = new Date(nMonthAheadDate).toISOString().split('T')[0]
+    return nMonthAheadDateFormatted;
+  }
+
+  const fetchData = useCallback(async (nMonths: number) => {
+    const formattedDate = calcDate(nMonths);
+    const apiUrl = 'http://localhost:3000/api/notes' + (nMonths !== 0 ? `/?from=${formattedDate}` : '');
     try {
-      const response = await fetch('http://localhost:3000/api/notes', {
+      const response = await fetch(apiUrl, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
@@ -28,7 +34,12 @@ function App() {
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchData(6);
+  }, [fetchData]);
+
   return (
     <div className="App">
       <NoteList notes={notesData}/>
